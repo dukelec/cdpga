6fa6e50bd41e159e7d65f7613ac716ce5004bd87
Duke Fong <duke@dukelec.com>
2018-09-03 14:29:34 +0800

    cdpga_p: add code

---
+++ cdpga_p/code/cdpga_p_drv/cdpga_p.c
@@ -0,0 +1,143 @@
+/*
+ * Software License Agreement (GPL License)
+ *
+ * Copyright (c) 2018, DUKELEC, Inc.
+ * All rights reserved.
+ *
+ * Author: Duke Fong <duke@dukelec.com>
+ */
+
+#define DEBUG
+
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/module.h>
+#include <linux/pci.h>
+
+#define DRV_NAME "cdpga_p"
+#define BAR_NUM 1
+
+typedef struct {
+    struct pci_dev  *dev;
+	void __iomem    *bar_va[BAR_NUM];
+} cdp_t;
+
+
+static int cdp_probe(struct pci_dev *dev, const struct pci_device_id *id)
+{
+    int ret, bar = 0;
+    cdp_t *cdp;
+
+    dev_dbg(&dev->dev, " %s\n", __FUNCTION__);
+
+    ret = pci_enable_device(dev);
+    if (ret) {
+        dev_err(&dev->dev, "pci_enable_device err: %d!\n", ret);
+        return ret;
+    }
+
+	cdp = kzalloc(sizeof(cdp_t), GFP_KERNEL);
+	if (!cdp) {
+		ret = -ENOMEM;
+		goto err_alloc;
+	}
+	
+    cdp->dev = dev;
+    pci_set_drvdata(dev, cdp);
+
+    ret = pci_enable_msi(dev);
+    if (ret) {
+        dev_err(&dev->dev, "pci_enable_msi err: %d!\n", ret);
+        goto err_msi;
+    }
+
+    ret = pci_request_regions(dev, DRV_NAME);
+    if (ret) {
+        dev_err(&dev->dev, "pci_request_regions err: %d!\n", ret);
+        goto err_regions;
+    }
+
+    cdp->bar_va[bar] = pci_iomap(dev, bar, 0);
+    if (!cdp->bar_va[bar]) {
+        dev_err(&dev->dev, "pci_iomap err!\n");
+        ret = -1;
+        goto err_map;
+    }
+
+    dev_dbg(&dev->dev, "cdp_probe successful\n");
+
+    {
+        uint32_t *reg_dir = cdp->bar_va[bar] + 4;
+
+        dev_dbg(&dev->dev, "%p: %08x\n", reg_dir, *reg_dir);
+        *reg_dir = 0xffff;
+        dev_dbg(&dev->dev, "%p: %08x\n", reg_dir, *reg_dir);
+    }
+
+    return 0;
+
+err_map:
+    pci_release_regions(dev);
+err_regions:
+    pci_disable_msi(dev);
+err_msi:
+    kfree(cdp);
+    pci_set_drvdata(dev, NULL);
+err_alloc:
+    pci_disable_device(dev);
+
+    return ret;
+}
+
+static void cdp_remove(struct pci_dev *dev)
+{
+    int bar = 0;
+    cdp_t *cdp = pci_get_drvdata(dev);
+
+    dev_dbg(&dev->dev, " %s\n", __FUNCTION__);
+
+    if (cdp->bar_va[bar]) {
+        pci_iounmap(dev, cdp->bar_va[bar]);
+        cdp->bar_va[bar] = NULL;
+    }
+
+    pci_release_regions(dev);
+    pci_disable_msi(dev);
+    pci_set_drvdata(dev, NULL);
+    pci_disable_device(dev);
+}
+
+
+static const struct pci_device_id ids[] = {
+    { PCI_DEVICE(0x1172, 0x0004), },
+    { 0, }
+};
+MODULE_DEVICE_TABLE(pci, ids);
+
+static struct pci_driver cdp_driver = {
+    .name = DRV_NAME,
+    .id_table = ids,
+    .probe = cdp_probe,
+    .remove = cdp_remove
+};
+
+static int __init cdp_init(void)
+{
+    int ret;
+    pr_debug(DRV_NAME " %s\n", __FUNCTION__);
+
+    ret = pci_register_driver(&cdp_driver);
+    if (ret < 0)
+        return ret;
+    return 0;
+}
+
+static void __exit cdp_exit(void)
+{
+    pr_debug(DRV_NAME " %s\n", __FUNCTION__);
+    pci_unregister_driver(&cdp_driver);
+}
+
+MODULE_LICENSE("GPL");
+module_init(cdp_init);
+module_exit(cdp_exit);
---
+++ cdpga_p/code/cdpga_p_fw/cdctl_pcie.v
@@ -0,0 +1,89 @@
+/*
+ * Software License Agreement (MIT License)
+ *
+ * Copyright (c) 2018, DUKELEC, Inc.
+ * All rights reserved.
+ *
+ * Author: Duke Fong <duke@dukelec.com>
+ */
+
+module cdctl_pcie(
+    input gclk,
+    input rst_n,
+    input pcie_refclk,
+    input pcie_rx,
+    output wire pcie_tx,
+    output wire clkreq_n,
+    output reg led0,
+    inout wire [15:0] pio_0_pins
+);
+
+assign clkreq_n = 1'b0;
+
+wire top_pll_50m;
+wire top_pll_125m;
+wire reconfig_busy;
+wire [3:0] reconfig_togxb_data;
+wire [4:0] reconfig_fromgxb_data;
+
+wire [63:0] test_out;
+wire [39:0] test_in;
+assign test_in[39:32] = 0;
+assign test_in[31:9] = 0;
+assign test_in[8:5] = 4'b0101;
+assign test_in[4:0] = 5'b01000;
+
+
+reg [22:0] counter;
+
+always @(posedge top_pll_50m or negedge top_pll_locked)
+    if (!top_pll_locked) begin
+        counter <= 0;
+        led0 <= 1;
+    end
+    else begin
+        counter <= counter + 1;
+        if (counter == 0) begin
+            led0 <= ~led0;
+        end
+    end
+
+
+altpll_ip altpll_top(
+    .inclk0(gclk),
+    .areset(~rst_n),
+    .c0(top_pll_125m),
+    .c1(top_pll_50m),
+    .locked(top_pll_locked));
+
+
+cdctl_sys cdctl_sys_m(
+    .pcie_ip_cal_blk_clk_clk(top_pll_50m),
+    .pcie_ip_fixedclk_clk(top_pll_125m),
+    .pcie_ip_pcie_rstn_export(rst_n),
+    .pcie_ip_pipe_ext_pipe_mode(1'b0),
+    
+    .pcie_ip_powerdown_pll_powerdown(~top_pll_locked),
+    .pcie_ip_powerdown_gxb_powerdown(~top_pll_locked),
+    
+    .pcie_ip_refclk_export(pcie_refclk),
+    .pcie_ip_rx_in_rx_datain_0(pcie_rx),
+    .pcie_ip_tx_out_tx_dataout_0(pcie_tx),
+    
+    .pcie_ip_test_in_test_in(test_in),
+    .pcie_ip_test_out_test_out(test_out),
+    
+    .pcie_ip_reconfig_gxbclk_clk(top_pll_50m),
+    .pcie_ip_reconfig_busy_busy_altgxb_reconfig(reconfig_busy),
+    .pcie_ip_reconfig_togxb_data(reconfig_togxb_data),
+    .pcie_ip_reconfig_fromgxb_0_data(reconfig_fromgxb_data),
+    
+    .pio_0_external_connection_export(pio_0_pins));
+
+altgxb_reconfig_ip altgxb_reconfig_m(
+    .reconfig_clk(top_pll_50m),
+    .reconfig_fromgxb(reconfig_fromgxb_data),
+    .busy(reconfig_busy),
+    .reconfig_togxb(reconfig_togxb_data));
+
+endmodule
